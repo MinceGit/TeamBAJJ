68bc32d3561567ca87ac957c3ac01d64caa1284f
jmho <justinho@ufl.edu>
2021-04-21 23:33:39 -0400

    Update firebase.js
    removed keys from public

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import 'firebase/firestore'
 
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyDWvsX_3hSoUuUfKIlKOl4iHcabqwwADMk",
-  authDomain: "ecouf-1bf8a.firebaseapp.com",
-  projectId: "ecouf-1bf8a",
-  storageBucket: "ecouf-1bf8a.appspot.com",
-  messagingSenderId: "139678000627",
-  appId: "1:139678000627:web:c365595c5e9cdf4b13de86",
-  measurementId: "G-T3GJF2LB1Z"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 })
 
 
